d1dd25821dcbb35f1a261c688c8b2f8b453e5edc
Tushar <tusharsingla22222@gmail.com>
2025-11-26 23:49:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -20,15 +20,15 @@ hamburger.addEventListener("click", () => {
 });
 
 
-window.addEventListener("scroll", () => {
-    const nav = document.querySelector("nav");
+// window.addEventListener("scroll", () => {
+//     const nav = document.querySelector("nav");
 
-    if (window.scrollY > 50) {
-        nav.classList.add("scrolled");
-    } else {
-        nav.classList.remove("scrolled");
-    }
-});
+//     if (window.scrollY > 50) {
+//         nav.classList.add("scrolled");
+//     } else {
+//         nav.classList.remove("scrolled");
+//     }
+// });
 
 
 
