e2ea475f3ed87b994150e9de354e80074c29b5b1
tusharsingla22222-source <tusharsingla22222@gmail.com>
2025-11-26 21:00:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+window.addEventListener("scroll", function() {
+    const nav = document.querySelector("nav");
+
+    if (window.scrollY > 400) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
+
+
+
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector("nav");
+
+    if (window.scrollY > 50) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
+
+
+
